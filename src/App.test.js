52f8f144ad4246b35a6b7c36032ae82b63ccaba5
Doniagv <doniagv@gmail.com>
2022-03-20 16:44:34 -0600

    #15 Se corrigio error npm build

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
-// import { render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 // import App from "./App";
+import Landingpage from "./components/Landingpage";
 
-test("Renders App", () => {
-  return true;
+test("Renders Landing Page", () => {
+  render(<Landingpage />);
 });
