b12360bf252aae10641201d78f0a3bb63f6b552e
javier-andoni <javier@radek.mx>
2022-05-12 13:11:14 -0500

    Se añadió seccion de busqueda de artistas como prueba para api spotify

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Landingpage from "./components/Landingpage";
 import Login from "./components/Login";
 import ResponsiveNavbar from "./components/Navbar";
+import Recomendacion from "./components/Recomendacion";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Landingpage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<Login />} />
+        <Route path="/roadtrip" element={<Recomendacion />} />
       </Routes>
     </>
   );
