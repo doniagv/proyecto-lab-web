8bfe525a455630250f4d4d17806595b6c40ffd31
javier-andoni <javier@radek.mx>
2022-06-08 13:09:36 -0500

    Se magrego _redirects y se corrigieron errores

--- src/components/FollowedArtists.js
+++ src/components/FollowedArtists.js
@@ -3,7 +3,7 @@ import React from "react";
 const FollowedArtists = ({ artist }) => {
   return (
     <div key={artist.id} className="m-2 text-center max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-900 dark:border-gray-700">
-      <a href={artist.uri}>{artist.images.length ? <img className="rounded-t-lg" src={artist.images[0].url} alt="" /> : <div>No Image</div>}</a>
+      <a href={artist.uri ? artist.uri : ""}>{artist.images.length ? <img className="rounded-t-lg" src={artist.images[0].url ? artist.images[0].url : ""} alt="" /> : <div>No Image</div>}</a>
       <div className="p-5">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{artist.name}</h5>
 
@@ -20,7 +20,7 @@ const FollowedArtists = ({ artist }) => {
 
         {/* <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{artist.genres.join(" ")}</p> */}
         <a
-          href={artist.uri}
+          href={artist.uri ? artist.uri : ""}
           className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-teal-700 rounded-lg hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800">
           Ver Artista
           <svg className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -3,7 +3,9 @@ import React from "react";
 const Tracks = ({ track }) => {
   return (
     <div key={track.id} className="m-2 text-center max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-900 dark:border-gray-700">
-      <a href={track.external_urls.spotify}>{track.album.images.length ? <img className="rounded-t-lg" src={track.album.images[0].url} alt="" /> : <div>No Image</div>}</a>
+      <a href={track.external_urls.spotify ? track.external_urls.spotify : ""}>
+        {track.album.images.length ? <img className="rounded-t-lg" src={track.album.images[0].url ? track.album.images[0].url : ""} alt="" /> : <div>No Image</div>}
+      </a>
       <div className="p-5">
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{track.name}</h5>
 
@@ -20,7 +22,7 @@ const Tracks = ({ track }) => {
         <a
           target="_blank"
           rel="noreferrer"
-          href={track.external_urls.spotify}
+          href={track.external_urls.spotify ? track.external_urls.spotify : ""}
           className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-teal-700 rounded-lg hover:bg-teal-800 focus:ring-4 focus:outline-none focus:ring-teal-300 dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800">
           Abrir Canción
           <svg className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
