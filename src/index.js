b5696b129300a02d815ee8ffcda31ae13853a926
javier-andoni <javier@radek.mx>
2022-06-08 12:26:19 -0500

    Se cambio a hashrouter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-  <BrowserRouter basename="/">
+  <HashRouter basename="/">
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 
   document.getElementById("root")
 );
